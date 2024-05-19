f48b0322f9479e3bd47efa1c87771ec55f19d960
alsiam09 <156769180+alsiam09@users.noreply.github.com>
2024-05-19 09:03:24 +0600

    Topupcenter

--- src/componant/Footer.jsx
+++ src/componant/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { MdCall } from "react-icons/md";
-
+import { FaWhatsapp } from "react-icons/fa6";
 const Footer = () => {
   return (
     <section className='bg-[#000] py-[50px]'>
@@ -18,7 +18,7 @@ const Footer = () => {
                 </a>
                 <a href="https://wa.me/message/3RGTSTCX3MO2G1" >
                 <div className="phonNumber mt-[10px] border-[2px] py-[5px] md:py-[10px] rounded-[20px] w-[300px] border-[#fff] items-center flex ">
-                    <div className="icon text-[#fff]  text-[30px] mx-[20px]"><MdCall /></div>
+                    <div className="icon text-[#fff]  text-[30px] mx-[20px]"><FaWhatsapp /></div>
                     <div className="TimeNum">
                         <span className='text-[12px] text-[#fff] font-[700]'>10:00AM-10.00PM</span>
                         <h2 className='text-[20px] md:text-[25px] text-[#fff] font-[400]'>01813904257</h2>
--- src/componant/Menu.jsx
+++ src/componant/Menu.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { FaBars } from "react-icons/fa";
 const Menu = () => {
+  let [Menu , setMenu ] = useState(false)
+  let MenuRef = useRef()
+  useState(()=>{
+    document.addEventListener("click" , (e)=>{
+      if (MenuRef.current.contains(e.target) == true) {
+        setMenu(!Menu)        
+      } else {
+        setMenu(false)
+      }
+    })
+  },[Menu])
   return (
     <section className=' fixed z-[998] top-[0px] left-0 w-[100%] bg-[#131313]'>
         <div className="container px-[10px] lg:px-[0px] justify-between flex h-[80px] mx-auto">
             <div className="sitename flex items-center md:justify-start">
                 <h1 className='md:text-[40px] text-[25px] text-[white] font-[700]'>albaba top up center</h1>
             </div>
-            <div className="menubtn md:text-[40px] text-[30px] flex items-center text-[#fff]">
-                <FaBars/>
+            <div ref={MenuRef} className="menubtn md:text-[40px] text-[30px] flex items-center text-[#fff]">
+                <FaBars />
             </div>
+            {Menu ? 
+                <ul className=' fixed w-[200px] lg:w-[300px]  duration-300 ease-in bg-[#000] h-[100%] top-[0px] right-0'>
+
+                </ul> :
+                <ul className=' fixed w-[200px] lg:w-[300px] bg-[#000] h-[100%] duration-300 ease-in top-[0px] right-[-200px] lg:right-[-300px]'>
+
+                </ul>}
         </div>
     </section>
   )
