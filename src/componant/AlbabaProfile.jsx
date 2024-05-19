88df13e83bdd9a6d2a0ced0a6ed2325803aeba84
alsiam09 <156769180+alsiam09@users.noreply.github.com>
2024-05-19 18:32:03 +0600

    Topupcenter

--- src/componant/AlbabaProfile.jsx
+++ src/componant/AlbabaProfile.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 import cover from '../assets/cover.jpg'
 import logo from '../assets/albabaLogo.png'
-import { IoIosArrowDown ,IoIosArrowUp  } from "react-icons/io";
+import { IoIosArrowDown ,IoIosArrowUp , IoIosCamera  } from "react-icons/io";
 const AlbabaProfile = () => {
   let [SD , setSD] = useState(false)
   let ClickSD =()=>{
     setSD(!SD)
+  }
+  let [Img , SetImg] = useState()
+  let ImgChang = (event) =>{
+    const file = event.target.files[0]
+    console.log(file);
+    SetImg(file)
   }
   return (
     <div className="main lg:border-r-[3px] lg:pr-[20px] px-[10px] lg:pl-[0px] border-r-[#fff]">
@@ -13,9 +19,12 @@ const AlbabaProfile = () => {
             <div className="coverImg overflow-hidden h-[200px]">
                 <img className='rounded-[10px]' src={cover} alt="" />
             </div>
-            <div className="logo w-[180px] bg-[#272727] p-[10px] rounded-[50%] absolute top-[100px] left-[20px]">
-                <img className='rounded-[50%]' src={logo} alt="" />
+            <div  className="logo w-[180px] h-[180px] bg-[#272727] p-[10px] rounded-[50%] absolute top-[100px] left-[20px]">
+                 {Img ? <img className='rounded-[60%] w-[170px] h-[160px] w-[160px] h-[170px]' src={URL.createObjectURL(Img)}/> :<img className=' w-[160px] h-[160px] rounded-[50%]' src={logo} alt="" />}
+                <div className="Active absolute left-[18px] bottom-[18px] w-[22px] h-[22px] rounded-[50%] bg-[green]"></div>
+                <label for="imgUp" className="Active absolute right-[1px] bottom-[20px] p-[10px] rounded-[50%] bg-[#272727] text-[#fff] text-[25px]"><IoIosCamera /></label>
             </div>
+            <input onChange={ImgChang} className=' hidden ' type="file" id="imgUp" />
         </div>
         <div className="MainBox">
         <div onClick={ClickSD} className=" userDetil my-[10px] rounded-[20px] border-[2px] items-center border-[#fff] w-[100%] h-[50px] flex justify-between px-[30px] bg-[#292929]">
