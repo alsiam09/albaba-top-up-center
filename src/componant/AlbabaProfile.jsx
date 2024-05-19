784c7eeeb3e097717e72db84de0f3a6e2eb8d84d
alsiam09 <156769180+alsiam09@users.noreply.github.com>
2024-05-19 17:34:07 +0600

    Topupcenter

--- src/componant/AlbabaProfile.jsx
+++ src/componant/AlbabaProfile.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cover from '../assets/cover.jpg'
 import logo from '../assets/albabaLogo.png'
+import { IoIosArrowDown ,IoIosArrowUp  } from "react-icons/io";
 const AlbabaProfile = () => {
+  let [SD , setSD] = useState(false)
+  let ClickSD =()=>{
+    setSD(!SD)
+  }
   return (
-    <div className="main">
-        <div className="imgBox lg:border-r-[3px]  border-r-[#fff] relative lg:pr-[20px] px-[10px] lg:pl-[0px] h-[280px]">
+    <div className="main lg:border-r-[3px] lg:pr-[20px] px-[10px] lg:pl-[0px] border-r-[#fff]">
+        <div className="imgBox  relative  h-[280px]">
             <div className="coverImg overflow-hidden h-[200px]">
                 <img className='rounded-[10px]' src={cover} alt="" />
             </div>
             <div className="logo w-[180px] bg-[#272727] p-[10px] rounded-[50%] absolute top-[100px] left-[20px]">
                 <img className='rounded-[50%]' src={logo} alt="" />
             </div>
+        </div>
+        <div className="MainBox">
+        <div onClick={ClickSD} className=" userDetil my-[10px] rounded-[20px] border-[2px] items-center border-[#fff] w-[100%] h-[50px] flex justify-between px-[30px] bg-[#292929]">
+          <h2 className='userName text-[25px] text-[white] font-bold'>alsiam</h2>
+          {SD == true ?  <div className="icon text-[25px] text-white"><IoIosArrowUp /></div> :  <div className="icon text-[25px] text-white"><IoIosArrowDown/></div>}
+        </div>
+        {SD == true && 
+           <ul className=" duration-300 ease-in DitelBox w-[100%] py-[30px] px-[30px] bg-[#292929]">
+            <li className='lg:text-[25px] text-[22px] font-font-name text-[white]'>User ID :<span className='pl-[10px] font-mono'>000001</span></li>
+            <li className='lg:text-[25px] text-[22px] font-font-name text-[white]'>Blance:<span className='pl-[10px] font-mono'>000001</span>৳</li>
+           </ul>
+        }
         </div>
     </div>
   )
--- src/componant/Menu.jsx
+++ src/componant/Menu.jsx
@@ -16,18 +16,22 @@ const Menu = () => {
     <section className=' fixed z-[998] top-[0px] left-0 w-[100%] bg-[#131313]'>
         <div className="container px-[10px] lg:px-[0px] justify-between flex h-[80px] mx-auto">
             <div className="sitename flex items-center md:justify-start">
-                <h1 className='md:text-[40px] text-[25px] text-[white] font-[700]'>albaba top up center</h1>
+                <h1 className='md:text-[40px] text-[25px] text-[white] font-[700] '>albaba top up center</h1>
             </div>
             <div ref={MenuRef} className="menubtn md:text-[40px] text-[30px] flex items-center text-[#fff]">
                 <FaBars />
             </div>
             {Menu ? 
-                <ul className=' fixed w-[200px] lg:w-[300px]  duration-300 ease-in bg-[#000] h-[100%] top-[0px] right-0'>
+                <div className=' fixed w-[200px] lg:w-[300px]  duration-300 ease-in bg-[#000] h-[100%] top-[0px] right-0'>
+                  <ul className='px-[10px] mt-[100px]'>
+                  <li className='text-[white] mb-[10px] text-[25px] lg:text-[30px] w-[100%] py-[5px] hover:bg-[#dbdbdb] hover:text-[#000] flex justify-start px-[20px] rounded-[20px] font-font-name'>Home</li>
+                  <li className='text-[white] mb-[10px] text-[25px] lg:text-[30px] w-[100%] py-[5px] hover:bg-[#dbdbdb] hover:text-[#000] flex justify-start px-[20px] rounded-[20px] font-font-name'>Account</li>
+                  <li className='text-[white] mb-[10px] text-[25px] lg:text-[30px] w-[100%] py-[5px] hover:bg-[#dbdbdb] hover:text-[#000] flex justify-start px-[20px] rounded-[20px] font-font-name'>Order</li>
+                  </ul>
+                </div> :
+                <div className=' fixed w-[200px] lg:w-[300px] bg-[#000] h-[100%] duration-300 ease-in top-[0px] right-[-200px] lg:right-[-300px]'>
 
-                </ul> :
-                <ul className=' fixed w-[200px] lg:w-[300px] bg-[#000] h-[100%] duration-300 ease-in top-[0px] right-[-200px] lg:right-[-300px]'>
-
-                </ul>}
+                </div>}
         </div>
     </section>
   )
